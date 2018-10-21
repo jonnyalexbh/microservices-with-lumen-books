725bece8ccd02b5566f590f80edeea64653f3e69
jonnyalexbh <jonnyalex.bh@gmail.com>
2018-10-20 22:16:05 -0500

    Creating a factory for books and generating random values

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -9,11 +9,13 @@
 | you a convenient way to create models for testing and seeding your
 | database. Just tell the factory how a default model should look.
 |
-*/
+ */
 
-$factory->define(App\User::class, function (Faker\Generator $faker) {
+$factory->define(App\Book::class, function (Faker\Generator $faker) {
     return [
-        'name' => $faker->name,
-        'email' => $faker->email,
+        'title' => $faker->sentence(3, true),
+        'description' => $faker->sentence(6, true),
+        'price' => $faker->numberBetween(25, 150),
+        'author_id' => $faker->numberBetween(1, 50),
     ];
 });
